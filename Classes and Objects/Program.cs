ab18f67788134a412ac81b31b2c7a96683fb85e5
hasher116 <andrei13081996@mail.ru>
2021-03-06 06:31:53 +0300

    Initial commit

---
+++ Classes and Objects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Classes_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Andrew = new Human(24, "Andrew");
+            Human Vasya = new Human {age = 25, name = "Vasya"};
+            Andrew.GetInfo();
+            Vasya.GetInfo();
+        }
+    }
+    class Human
+    {
+        public int age = 0;
+        public string name = "Andrew";
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"age={age}\nname={name}");
+        }
+        public Human()
+        {
+
+        }
+        public Human(int age)
+        {
+            this.age = age;
+        }
+        public Human(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+    }
+}
